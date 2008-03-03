a1277c391edfdba65b20712d3fc076db48100178
Patrick Mackaaij <patrick@mackaaij.org>
2008-03-03 16:19:00 +0100

    Process file droppen on the script

--- Create Excel System DSN.bat
+++ Create Excel System DSN.bat
@@ -1,31 +1,26 @@
 @echo off
-REM Batch file to list all the Excel files in the current directory and configure System DSN's for each of them
-REM Patrick Mackaaij, 03-03-2008 v1.00
+REM Batch file to configure a System DSN for a Excel file dropped on this script
+REM Patrick Mackaaij, 03-03-2008 v1.1
 REM
-REM Parameters not passed at the moment:
-REM - Default directory without trailing slash (DefaultDir)
+REM Parameters not passed to the System DSN at the moment:
 REM - Excel version (FIL)
+REM - Default directory without trailing slash (DefaultDir)
 
-REM =========================================== START Main code
-REM List all .xls files in the current directory using: 'dir /b /on *.xls'
-REM Pass the filenames (without extension) to: createDSN
-for /f %%i in ('dir /b /on *.xls') do (set currentFilename=%%~ni) & (set currentFullname=%%~fi) & call :createDSN
+REM =========================================== START Check code
+REM Check wether the dropped file is an Excel file (based on the extension ".xls")
+if NOT "%~x1"==".xls" echo Received filename: %1 & echo This filename does not have the extension ".xls". & echo This file is not expected to be an Excel file so the script will be aborted. & color 4f & pause & goto :eof
+REM =========================================== END Check code
 
-REM Quit after the for-loop
-goto :quit
-REM =========================================== END Main code
+REM =========================================== START CreateDSN code
+set Filename=%~n1
+set Fullname=%~f1
 
-REM =========================================== START CreateDSN loop code
-:createDSN
 REM Configure System DSN using command line tool odbcconf
 REM The filename is used as the DSN name
 REM If a DSN name already exists it is overwritten
 REM DSN is created as ReadOnly
-odbcconf configsysdsn "Microsoft Excel Driver (*.xls)" "DSN=%currentFilename%|DBQ=%currentFullname%|ReadOnly=01|Description=Configure System DSN's for Excel files (ODBC)"
-
-REM Exit this loop
-goto :eof
-REM =========================================== END CreateDSN loop code
+odbcconf configsysdsn "Microsoft Excel Driver (*.xls)" "DSN=%Filename%|DBQ=%Fullname%|ReadOnly=01|Description=Configure System DSN's for Excel files (ODBC)"
+REM =========================================== END CreateDSN code
 
 REM =========================================== START End code
 :quit
